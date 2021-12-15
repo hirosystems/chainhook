00e332820441b851e8c60da34184e83bbe25daf5
Ludo Galabru <ludovic@blockstack.com>
2021-12-15 15:54:34 -0500

    feat: update default costs

--- src/lib.rs
+++ src/lib.rs
@@ -60,9 +60,10 @@ pub async fn init_session(fetch_contract: String) -> String {
                         cache: None,
                     }]
                 };
+                let default_costs_version = 2;
                 let mut settings = SessionSettings::default();
-                settings.include_boot_contracts = vec!["costs-v1".into()];
-                settings.costs_version = 1;
+                settings.include_boot_contracts = vec![format!("costs-v{}", default_costs_version)];
+                settings.costs_version = default_costs_version;
                 settings.initial_links = initial_links;
                 let mut session = Session::new(settings);
                 let output = session.start_wasm().await;
