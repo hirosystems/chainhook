2e1c5b02a375b72358487400ddb428a693dd10f3
MicaiahReid <micaiahreid@gmail.com>
2023-07-31 16:05:38 -0400

    add test cases to predicate print_event

--- components/chainhook-sdk/src/chainhooks/tests/mod.rs
+++ components/chainhook-sdk/src/chainhooks/tests/mod.rs
@@ -5,22 +5,150 @@ use super::{
 use crate::chainhooks::types::{HookAction, StacksPredicate};
 use crate::utils::Context;
 use chainhook_types::StacksNetwork;
-use chainhook_types::{StacksBlockUpdate, StacksChainEvent, StacksChainUpdatedWithBlocksData};
+use chainhook_types::{StacksBlockUpdate, StacksChainEvent, StacksChainUpdatedWithBlocksData, SmartContractEventData};
+use test_case::test_case;
 
 pub mod fixtures;
 
-#[test]
-fn test_stacks_predicate_print_event() {
+static PRINT_EVENT_HEX: &str = "0x0c00000002077061796c6f61640c0000000204646174610c0000000d03617072010000000000000000000000000000000c056173736574061ad5d891cb8b4c37b1f6d7d10c093aaeb7c6fad0f00f577261707065642d426974636f696e08626f72726f776572051ac3ff90270cdae87cf86d1b0470e6eeb203f134de0a636f6c6c2d726174696f01000000000000000000000000000000000a636f6c6c2d746f6b656e061ad5d891cb8b4c37b1f6d7d10c093aaeb7c6fad0f00f577261707065642d426974636f696e0a636f6c6c2d7661756c74061ad5d891cb8b4c37b1f6d7d10c093aaeb7c6fad0f00a636f6c6c2d7661756c7407637265617465640100000000000000000000000000252ead0d66756e64696e672d7661756c74061ad5d891cb8b4c37b1f6d7d10c093aaeb7c6fad0f00d66756e64696e672d7661756c740b6c6f616e2d616d6f756e7401000000000000000000000000000017700c6e6578742d7061796d656e7401000000000000000000000000000000000e7061796d656e742d706572696f64010000000000000000000000000000001e1272656d61696e696e672d7061796d656e7473010000000000000000000000000000000c06737461747573020000000101036b6579010000000000000000000000000000000b04747970650d000000087365742d6c6f616e";
+
+static EMPTY_EVENT_HEX: &str = "0x0d00000000";
+#[test_case(
+    vec![
+        vec![
+            SmartContractEventData {
+                topic: "print".to_string(),
+                contract_identifier: "ST3AXH4EBHD63FCFPTZ8GR29TNTVWDYPGY0KDY5E5.loan-data".to_string(),
+                hex_value: PRINT_EVENT_HEX.to_string()
+            }
+        ]
+    ], 
+    StacksPrintEventBasedPredicate {
+        contract_identifier: Some(
+            "ST3AXH4EBHD63FCFPTZ8GR29TNTVWDYPGY0KDY5E5.loan-data".to_string(),
+        ),
+        contains: Some("set-loan".to_string()),
+    },
+    1;
+    "matches contract_identifier and contains"
+)]
+#[test_case(
+    vec![
+        vec![
+            SmartContractEventData {
+                topic: "print".to_string(),
+                contract_identifier: "no-match".to_string(),
+                hex_value: PRINT_EVENT_HEX.to_string()
+            }
+        ]
+    ], 
+    StacksPrintEventBasedPredicate {
+        contract_identifier: Some(
+            "ST3AXH4EBHD63FCFPTZ8GR29TNTVWDYPGY0KDY5E5.loan-data".to_string(),
+        ),
+        contains: Some("set-loan".to_string()),
+    }, 
+    0;
+    "rejects non matching contract_identifier"
+)]
+#[test_case(
+    vec![
+        vec![
+            SmartContractEventData {
+                topic: "print".to_string(),
+                contract_identifier: "ST3AXH4EBHD63FCFPTZ8GR29TNTVWDYPGY0KDY5E5.loan-data".to_string(),
+                hex_value: EMPTY_EVENT_HEX.to_string()
+            }
+        ]
+    ], 
+    StacksPrintEventBasedPredicate {
+        contract_identifier: Some(
+            "ST3AXH4EBHD63FCFPTZ8GR29TNTVWDYPGY0KDY5E5.loan-data".to_string(),
+        ),
+        contains: Some("set-loan".to_string()),
+    }, 
+    0;
+    "rejects non matching contains value"
+)]
+#[test_case(
+    vec![
+        vec![
+            SmartContractEventData {
+                topic: "print".to_string(),
+                contract_identifier: "ST3AXH4EBHD63FCFPTZ8GR29TNTVWDYPGY0KDY5E5.loan-data".to_string(),
+                hex_value: PRINT_EVENT_HEX.to_string()
+            }
+        ]
+    ], 
+    StacksPrintEventBasedPredicate {
+        contract_identifier: None,
+        contains: Some("set-loan".to_string()),
+    }, 
+    1;
+    "ommitting contract_identifier checks all print events for match"
+)]
+#[test_case(
+    vec![
+        vec![
+            SmartContractEventData {
+                topic: "print".to_string(),
+                contract_identifier: "ST3AXH4EBHD63FCFPTZ8GR29TNTVWDYPGY0KDY5E5.loan-data".to_string(),
+                hex_value: EMPTY_EVENT_HEX.to_string()
+            }
+        ],
+        vec![
+            SmartContractEventData {
+                topic: "print".to_string(),
+                contract_identifier: "".to_string(),
+                hex_value: EMPTY_EVENT_HEX.to_string()
+            }
+        ]
+    ], 
+    StacksPrintEventBasedPredicate {
+        contract_identifier: Some(
+            "ST3AXH4EBHD63FCFPTZ8GR29TNTVWDYPGY0KDY5E5.loan-data".to_string(),
+        ),
+        contains: None,
+    }, 
+    1;
+    "ommitting contains matches all values for matching events"
+)]
+#[test_case(
+    vec![
+        vec![
+            SmartContractEventData {
+                topic: "print".to_string(),
+                contract_identifier: "ST3AXH4EBHD63FCFPTZ8GR29TNTVWDYPGY0KDY5E5.loan-data".to_string(),
+                hex_value: EMPTY_EVENT_HEX.to_string()
+            }
+        ],
+        vec![
+            SmartContractEventData {
+                topic: "print".to_string(),
+                contract_identifier: "".to_string(),
+                hex_value: PRINT_EVENT_HEX.to_string()
+            }
+        ]
+    ], 
+    StacksPrintEventBasedPredicate {
+        contract_identifier: None,
+        contains: None,
+    }, 
+    2;
+    "ommitting contract_identifier and contains matches all values on all print events"
+)]
+
+fn test_stacks_predicate_print_event(blocks_with_events: Vec<Vec<SmartContractEventData>>, predicate_event: StacksPrintEventBasedPredicate, expected_applies: u64) {
     // Prepare block
+    let new_blocks = blocks_with_events.iter().map(|events| StacksBlockUpdate {
+        block: fixtures::build_stacks_testnet_block_from_smart_contract_event_data(events),
+        parent_microblocks_to_apply: vec![],
+        parent_microblocks_to_rollback: vec![],
+    }).collect();
     let event = StacksChainEvent::ChainUpdatedWithBlocks(StacksChainUpdatedWithBlocksData {
-        new_blocks: vec![StacksBlockUpdate {
-            block: fixtures::get_stacks_testnet_block(107605).clone(),
-            parent_microblocks_to_apply: vec![],
-            parent_microblocks_to_rollback: vec![],
-        }],
+        new_blocks,
         confirmed_blocks: vec![],
     });
-
     // Prepare predicate
     let print_predicate = StacksChainhookSpecification {
         uuid: "".to_string(),
@@ -29,17 +157,12 @@ fn test_stacks_predicate_print_event() {
         network: StacksNetwork::Testnet,
         version: 1,
         blocks: None,
-        start_block: Some(107604),
-        end_block: Some(107607),
+        start_block: None,
+        end_block: None,
         expire_after_occurrence: None,
         capture_all_events: None,
         decode_clarity_values: None,
-        predicate: StacksPredicate::PrintEvent(StacksPrintEventBasedPredicate {
-            contract_identifier: Some(
-                "ST3AXH4EBHD63FCFPTZ8GR29TNTVWDYPGY0KDY5E5.loan-data".to_string(),
-            ),
-            contains: Some("set-loan".to_string()),
-        }),
+        predicate: StacksPredicate::PrintEvent(predicate_event),
         action: HookAction::Noop,
         enabled: true,
     };
@@ -47,5 +170,12 @@ fn test_stacks_predicate_print_event() {
     let predicates = vec![&print_predicate];
     let (triggered, _blocks) =
         evaluate_stacks_chainhooks_on_chain_event(&event, predicates, &Context::empty());
-    assert_eq!(triggered.len(), 1);
+
+    if expected_applies == 0 {
+        assert_eq!(triggered.len(), 0)
+    }
+    else {
+        let actual_applies: u64 = triggered[0].apply.len().try_into().unwrap();
+        assert_eq!(actual_applies, expected_applies);
+    }
 }
