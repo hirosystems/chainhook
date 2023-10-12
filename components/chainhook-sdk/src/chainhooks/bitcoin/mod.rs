6d63b1f25de52c9ff9490e64ac6674cabc6efbf7
Ludo Galabru <ludo@hiro.so>
2023-10-11 20:52:04 -0400

    refactor: simplify model

--- components/chainhook-sdk/src/chainhooks/bitcoin/mod.rs
+++ components/chainhook-sdk/src/chainhooks/bitcoin/mod.rs
@@ -26,25 +26,20 @@ pub struct BitcoinTriggerChainhook<'a> {
     pub rollback: Vec<(Vec<&'a BitcoinTransactionData>, &'a BitcoinBlockData)>,
 }
 
-#[derive(Clone, Debug, Serialize)]
-pub struct BitcoinApplyTransactionPayload {
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct BitcoinTransactionPayload {
     pub block: BitcoinBlockData,
 }
 
-#[derive(Clone, Debug, Serialize)]
-pub struct BitcoinRollbackTransactionPayload {
-    pub block: BitcoinBlockData,
-}
-
-#[derive(Clone, Debug)]
+#[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct BitcoinChainhookPayload {
     pub uuid: String,
 }
 
-#[derive(Clone, Debug)]
+#[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct BitcoinChainhookOccurrencePayload {
-    pub apply: Vec<BitcoinApplyTransactionPayload>,
-    pub rollback: Vec<BitcoinRollbackTransactionPayload>,
+    pub apply: Vec<BitcoinTransactionPayload>,
+    pub rollback: Vec<BitcoinTransactionPayload>,
     pub chainhook: BitcoinChainhookPayload,
 }
 
@@ -280,7 +275,7 @@ pub fn handle_bitcoin_hook_action<'a>(
                             .into_iter()
                             .map(|t| t.clone())
                             .collect::<Vec<_>>();
-                        BitcoinApplyTransactionPayload { block }
+                        BitcoinTransactionPayload { block }
                     })
                     .collect::<Vec<_>>(),
                 rollback: trigger
@@ -292,7 +287,7 @@ pub fn handle_bitcoin_hook_action<'a>(
                             .into_iter()
                             .map(|t| t.clone())
                             .collect::<Vec<_>>();
-                        BitcoinRollbackTransactionPayload { block }
+                        BitcoinTransactionPayload { block }
                     })
                     .collect::<Vec<_>>(),
                 chainhook: BitcoinChainhookPayload {
