b97c3470d0bd6864491302f89676a1a99d2aeffd
MicaiahReid <micaiahreid@gmail.com>
2024-01-10 16:54:49 -0500

    add server to serve prometheus metrics

--- components/chainhook-sdk/src/monitoring.rs
+++ components/chainhook-sdk/src/monitoring.rs
@@ -1,3 +1,11 @@
+use crate::utils::Context;
+
+use hiro_system_kit::slog;
+use hyper::{
+    header::CONTENT_TYPE,
+    service::{make_service_fn, service_fn},
+    Body, Method, Request, Response, Server,
+};
 use prometheus::{
     self,
     core::{AtomicU64, GenericGauge},
@@ -243,6 +251,70 @@ impl PrometheusMonitoring {
                 "deregistered_predicates": self.stx_deregistered_predicates.get(),
             }
         })
+    }
+}
+
+async fn serve_req(
+    req: Request<Body>,
+    registry: Registry,
+    ctx: Context,
+) -> Result<Response<Body>, hyper::Error> {
+    match (req.method(), req.uri().path()) {
+        (&Method::GET, "/metrics") => {
+            ctx.try_log(|logger| {
+                slog::debug!(
+                    logger,
+                    "Prometheus monitoring: respsonding to metrics request"
+                )
+            });
+
+            let encoder = TextEncoder::new();
+            let metric_families = registry.gather();
+            let mut buffer = vec![];
+            encoder.encode(&metric_families, &mut buffer).unwrap();
+
+            let response = Response::builder()
+                .status(200)
+                .header(CONTENT_TYPE, encoder.format_type())
+                .body(Body::from(buffer))
+                .unwrap();
+
+            Ok(response)
+        }
+        (_, _) => {
+            ctx.try_log(|logger| {
+                slog::debug!(
+                    logger,
+                    "Prometheus monitoring: received request with invalid method/route: {}/{}",
+                    req.method(),
+                    req.uri().path()
+                )
+            });
+            let response = Response::builder().status(404).body(Body::empty()).unwrap();
+
+            Ok(response)
+        }
+    }
+}
+
+pub async fn start_serving_prometheus_metrics(port: u16, registry: Registry, ctx: Context) {
+    let addr = ([127, 0, 0, 1], port).into();
+    let ctx_clone = ctx.clone();
+    let make_svc = make_service_fn(|_| {
+        let registry = registry.clone();
+        let ctx_clone = ctx_clone.clone();
+        async move {
+            Ok::<_, hyper::Error>(service_fn(move |r| {
+                serve_req(r, registry.clone(), ctx_clone.clone())
+            }))
+        }
+    });
+    let serve_future = Server::bind(&addr).serve(make_svc);
+
+    ctx.try_log(|logger| slog::info!(logger, "Prometheus monitoring: listening on port {}", port));
+
+    if let Err(err) = serve_future.await {
+        ctx.try_log(|logger| slog::warn!(logger, "Prometheus monitoring: server error: {}", err));
     }
 }
 
