ad101076e2a7bec0a30f0488626ce80f40476e66
Rafael Cárdenas <rafael@rafaelcr.com>
2024-04-23 09:17:33 -0600

    feat: add Bitcoin transaction index to typescript client (#568)
    Also separate `StacksPayload` and `BitcoinPayload` into separate types
    for easier client-side inference.
    Bump version to 1.8.0

--- components/client/typescript/src/schemas/payload.ts
+++ components/client/typescript/src/schemas/payload.ts
@@ -1,18 +1,6 @@
 import { Static, Type } from '@sinclair/typebox';
-import { StacksEventSchema } from './stacks/payload';
-import { BitcoinEventSchema } from './bitcoin/payload';
-import { BitcoinIfThisSchema } from './bitcoin/if_this';
-import { StacksIfThisSchema } from './stacks/if_this';
+import { StacksPayloadSchema } from './stacks/payload';
+import { BitcoinPayloadSchema } from './bitcoin/payload';
 
-const EventArray = Type.Union([Type.Array(StacksEventSchema), Type.Array(BitcoinEventSchema)]);
-
-export const PayloadSchema = Type.Object({
-  apply: EventArray,
-  rollback: EventArray,
-  chainhook: Type.Object({
-    uuid: Type.String(),
-    predicate: Type.Union([BitcoinIfThisSchema, StacksIfThisSchema]),
-    is_streaming_blocks: Type.Boolean(),
-  }),
-});
+export const PayloadSchema = Type.Union([BitcoinPayloadSchema, StacksPayloadSchema]);
 export type Payload = Static<typeof PayloadSchema>;
