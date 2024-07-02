e13988a16613d8766c60942efbff8dc9a38a377a
brady.ouren <bouren@hiro.sh>
2024-07-01 18:35:53 -0700

    use chainhooks::types PoxConfig and deprecate the stacks indexer impl

--- components/chainhook-sdk/src/chainhooks/types.rs
+++ components/chainhook-sdk/src/chainhooks/types.rs
@@ -579,6 +579,7 @@ pub fn get_stacks_canonical_magic_bytes(network: &BitcoinNetwork) -> [u8; 2] {
     }
 }
 
+#[derive(Deserialize, Debug, Clone)]
 pub struct PoxConfig {
     pub genesis_block_height: u64,
     pub prepare_phase_len: u64,
@@ -608,21 +609,21 @@ impl PoxConfig {
     }
 }
 
-const POX_CONFIG_MAINNET: PoxConfig = PoxConfig {
+pub const POX_CONFIG_MAINNET: PoxConfig = PoxConfig {
     genesis_block_height: 666050,
     prepare_phase_len: 100,
     reward_phase_len: 2100,
     rewarded_addresses_per_block: 2,
 };
 
-const POX_CONFIG_TESTNET: PoxConfig = PoxConfig {
+pub const POX_CONFIG_TESTNET: PoxConfig = PoxConfig {
     genesis_block_height: 2000000,
     prepare_phase_len: 50,
     reward_phase_len: 1050,
     rewarded_addresses_per_block: 2,
 };
 
-const POX_CONFIG_DEVNET: PoxConfig = PoxConfig {
+pub const POX_CONFIG_DEVNET: PoxConfig = PoxConfig {
     genesis_block_height: 100,
     prepare_phase_len: 5,
     reward_phase_len: 20,
